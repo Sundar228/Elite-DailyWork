7558d7c951d2c486fda3f328bacbb4acd5ef16b5
Talla Ganesh Venkata Sundar <84611208+Sundar228@users.noreply.github.com>
2023-01-28 15:31:37 +0530

    Add files via upload

---
+++ Range sum of BST.java
@@ -0,0 +1,16 @@
+class Solution {
+    int sum=0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null){
+            return 0;
+        }
+        rangeSumBST(root.left,low,high);
+        if(root.val>=low && root.val<=high){
+            
+            sum+=root.val;
+            System.out.println(sum);
+        }
+        rangeSumBST(root.right,low,high);
+        return sum;
+    }
+}
